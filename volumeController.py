3151747774a42054d935de6e068a000391dfdc32
thisaramanohara <thisaramanohara@gmail.com>
2021-09-02 01:54:40 +0530

    Minimum volume and maximum volume obtained

--- volumeController.py
+++ volumeController.py
@@ -3,6 +3,10 @@ import time
 import numpy as np
 import math
 
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 import handTrackingModule as htm
 
 wCam, hCam = 640, 480
@@ -14,6 +18,20 @@ cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(0, None)
+
+minVol = volRange[0]
+maxVol  =volRange[1]
+
+
 
 
 while True:
